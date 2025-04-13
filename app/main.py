8e340e61577f6bb064130ee774163563c1430cdc
Nazar Slavych <slavuch232355@gmail.com>
2025-04-13 17:47:21 +0300

    develop

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 import os
 import requests
 
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
 
-def get_weather(city="Paris") -> None:
+def get_weather(city="Paris"):
     api_key = os.getenv("API_KEY")
     if not api_key:
         raise ValueError("API_KEY environment variable is not set")
 
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+    url = f"{BASE_URL}?key={api_key}&q={city}"
     response = requests.get(url)
     data = response.json()
 
@@ -18,7 +19,6 @@ def get_weather(city="Paris") -> None:
     localtime = data["location"]["localtime"]
 
     print(f"{location}/{country} {localtime} Weather: {temp_c} Celsius, {condition}")
-
 
 if __name__ == "__main__":
     get_weather()
